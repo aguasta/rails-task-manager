c1956a60561eb7845c23e2965af259643be52676
Aymeric Guastavino <aymeric.guastavino@gmail.com>
2021-02-09 12:04:38 +0100

    add

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,10 @@
 class Task < ApplicationRecord
     validates :title, presence: true
-end
+    after_initialize :default_values
+
+  
+    private
+      def default_values
+        task.completed ||= false
+      end
+  end
